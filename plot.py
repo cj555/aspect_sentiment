6fbcb3650cc324357772514d438f2335b3c26836
cj555 <chengjuan555@gmail.com>
2019-04-22 15:52:47 +0800

    Update plot.py

--- plot.py
+++ plot.py
@@ -1,3 +1,13 @@
+from sklearn.manifold import TSNE
+import matplot.pylab as plt
 def plot_tsne(arr):
-    for X in arr:
+    """
+        arr = [np.array([[0, 0, 0], [0, 1, 1], [1, 0, 1], [1, 1, 1]]),
+           np.array([[0, 0, 0], [0, 1, 3], [1, 4, 1], [1, 1, 1]])]
+    :param arr:
+    :return:
+    """
+    for i,X in enumerate(arr):
         X_embedded = TSNE(n_components=2).fit_transform(X)
+        plt.plot(X,label=i)
+    plt.save_figure('tsne.png')
