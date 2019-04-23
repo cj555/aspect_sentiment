91b26a5fc22bfd70b4ff50993ad63d9ccff2e3a0
cj555 <chengjuan555@gmail.com>
2019-04-23 09:18:05 +0800

    bailin model

--- model.py
+++ model.py
@@ -1,13 +1,18 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+import torch.optim as optim
+from torch.autograd import Variable
+import pdb
+import pickle
+from CRF import LinearCRF
+import torch.nn.init as init
+import pdb
+import pickle
+import numpy as np
+from Layer import SimpleCat
 from torch.nn import utils as nn_utils
 from util import *
-from torch.nn import utils as nn_utils
-import pickle
-from torch.autograd import Variable
-
-import torch.nn.init as init
 
 
 def init_ortho(module):
@@ -16,11 +21,37 @@ def init_ortho(module):
             init.orthogonal_(weight_)
 
 
-class MLSTM(nn.Module):
+class LSTM(nn.Module):
     def __init__(self, config):
-        super(MLSTM, self).__init__()
+        super(LSTM, self).__init__()
         self.config = config
-        # The concatenated word embedding and target embedding as input
+
+        self.rnn = nn.LSTM(config.embed_dim + config.mask_dim, config.l_hidden_size, batch_first=True,
+                           num_layers=int(config.l_num_layers / 2),
+                           bidirectional=False, dropout=config.l_dropout)
+        init_ortho(self.rnn)
+
+    # batch_size * sent_l * dim
+    def forward(self, feats, seq_lengths=None):
+        '''
+        Args:
+        feats: batch_size, max_len, emb_dim
+        seq_lengths: batch_size
+        '''
+        pack = nn_utils.rnn.pack_padded_sequence(feats,
+                                                 seq_lengths, batch_first=True)
+
+        # batch_size*max_len*hidden_dim
+        lstm_out, (h, c) = self.rnn(pack)
+        # batch_size*emb_dim
+        return h[0]
+
+
+class biLSTM(nn.Module):
+    def __init__(self, config):
+        super(biLSTM, self).__init__()
+        self.config = config
+
         self.rnn = nn.LSTM(config.embed_dim + config.mask_dim, int(config.l_hidden_size / 2), batch_first=True,
                            num_layers=int(config.l_num_layers / 2),
                            bidirectional=True, dropout=config.l_dropout)
@@ -33,150 +64,195 @@ class MLSTM(nn.Module):
         feats: batch_size, max_len, emb_dim
         seq_lengths: batch_size
         '''
-        # FIXIT: doesn't have batch
-        # Sort the lengths
         pack = nn_utils.rnn.pack_padded_sequence(feats,
                                                  seq_lengths, batch_first=True)
 
-        # assert self.batch_size == batch_size
+        # batch_size*max_len*hidden_dim
         lstm_out, _ = self.rnn(pack)
-        # lstm_out, (hid_states, cell_states) = self.rnn(feats)
-
         # Unpack the tensor, get the output for varied-size sentences
+        # padding with zeros
         unpacked, _ = nn_utils.rnn.pad_packed_sequence(lstm_out, batch_first=True)
+        # batch * sent_l * 2 * hidden_states
         return unpacked
 
 
-# input layer for 14
-class SimpleCat(nn.Module):
+# consits of three components
+class CRFAspectSent(nn.Module):
     def __init__(self, config):
         '''
-        Concatenate word embeddings and target embeddings
+        LSTM+Aspect
         '''
-        super(SimpleCat, self).__init__()
+        super(CRFAspectSent, self).__init__()
         self.config = config
-        with open(self.config.embed_path, 'rb') as f:
-            vectors = pickle.load(f).local_emb
 
-        self.local_emb = vectors
-        self.word_embed = nn.Embedding(vectors.shape[0], vectors.shape[1])
-        self.mask_embed = nn.Embedding(2, 50)
+        self.bilstm = biLSTM(config)
+        self.feat2tri = nn.Linear(config.l_hidden_size, 2)
+        self.inter_crf = LinearCRF(config)
+        self.feat2label = nn.Linear(config.l_hidden_size, 3)
 
-        # positional embeddings
-        # n_position = 100
-        # self.position_enc = nn.Embedding(n_position, config.embed_dim, padding_idx=0)
-        # # self.position_enc.weight.data = self.position_encoding_init(n_position, config.embed_dim)
-        #
-        self.dropout = nn.Dropout(0.1)
-        #
-        # self.senti_embed = nn.Embedding(config.embed_num, 50)
+        self.loss = nn.NLLLoss()
+        self.cat_layer = SimpleCat(config)
+        # Modified by Richard Sun
 
-        # input are tensors
-    def forward(self, sent, mask, is_elmo=False):
+    def compute_scores(self, sents, masks, lens):
         '''
         Args:
-        sent: tensor, shape(batch_size, max_len, emb_dim)
-        mask: tensor, shape(batch_size, max_len)
+        sents: batch_size*max_len*word_dim
+        masks: batch_size*max_len
+        lens: batch_size
         '''
-        # Modified by Richard Sun
-        # Use ELmo embedding, note we need padding
-        sent = Variable(sent)
-        mask = Variable(mask)
 
-        # Use GloVe embedding
-        if self.config.if_gpu:
-            sent, mask = sent.cuda(), mask.cuda()
-        # to embeddings
-        sent_vec = sent  # batch_siz*sent_len * dim
-        if is_elmo:
-            sent_vec = sent  # batch_siz*sent_len * dim
-        else:
-            sent_vec = self.word_embed(sent)  # batch_siz*sent_len * dim
+        # Context embeddings
+        context = self.bilstm(sents, lens)  # Batch_size*sent_len*hidden_dim
+        batch_size, max_len, hidden_dim = context.size()
+        # Target embeddings
+        # Find target indices, a list of indices
+        target_indices, target_max_len = convert_mask_index(masks)
 
-        mask_vec = self.mask_embed(mask)  # batch_size*max_len* dim
-        # print(mask_vec.size())
+        # Find the target context embeddings, batch_size*max_len*hidden_size
+        masks = masks.type_as(context)
+        masks = masks.expand(hidden_dim, batch_size, max_len).transpose(0, 1).transpose(1, 2)
+        target_emb = masks * context
 
-        sent_vec = self.dropout(sent_vec)
-        # Concatenation
-        sent_vec = torch.cat([sent_vec, mask_vec], 2)
+        target_emb_avg = torch.sum(target_emb, 1) / torch.sum(masks, 1)  # Batch_size*embedding
+        # Expand dimension for concatenation
+        target_emb_avg_exp = target_emb_avg.expand(max_len, batch_size, hidden_dim)
+        target_emb_avg_exp = target_emb_avg_exp.transpose(0, 1)  # Batch_size*max_len*embedding
 
-        # for test
-        return sent_vec
+        context = context + target_emb_avg_exp
 
-    def load_vector(self):
+        tri_scores = self.feat2tri(context)  # Batch_size*sent_len*2
+
+        # Take target embedding into consideration
+
+
+
+        marginals = []
+        select_polarities = []
+        label_scores = []
+        # Sentences have different lengths, so deal with them one by one
+        for i, tri_score in enumerate(tri_scores):
+            sent_len = lens[i].cpu().item()
+            if sent_len > 1:
+                tri_score = tri_score[:sent_len, :]  # sent_len, 2
+            else:
+                print('Too short sentence')
+            marginal = self.inter_crf(tri_score)  # sent_len, latent_label_size
+            # Get only the positive latent factor
+            select_polarity = marginal[:, 1]  # sent_len, select only positive ones
+
+            marginal = marginal.transpose(0, 1)  # 2 * sent_len
+            sent_v = torch.mm(select_polarity.unsqueeze(0),
+                              context[i, :sent_len, :])  # 1*sen_len, sen_len*hidden_dim=1*hidden_dim
+            label_score = self.feat2label(sent_v).squeeze(0)  # label_size
+            label_scores.append(label_score)
+            select_polarities.append(select_polarity)
+            marginals.append(marginal)
+
+        label_scores = torch.stack(label_scores)
+
+        return label_scores, select_polarities
+
+    def compute_predict_scores(self, sents, masks, lens):
         '''
-        Load pre-savedd word embeddings
+        Args:
+        sents: batch_size*max_len*word_dim
+        masks: batch_size*max_len
+        lens: batch_size
         '''
-        with open(self.config.embed_path, 'rb') as f:
-            vectors = pickle.load(f).local_emb
 
-            print("Loaded from {} with shape {}".format(self.config.embed_path, vectors.shape))
-            self.word_embed.weight = nn.Parameter(torch.FloatTensor(vectors))
-            # vectors = self.config.local_emb
-            self.word_embed.weight.data.copy_(torch.from_numpy(vectors))
-            # self.word_embed.weight.requires_grad = self.config.if_update_embed
-            self.word_embed.weight.requires_grad = False
+        context = self.bilstm(sents, lens)  # Batch_size*sent_len*hidden_dim
+        batch_size, max_len, hidden_dim = context.size()
+        # Target embeddings
+        # Find target indices, a list of indices
+        target_indices, target_max_len = convert_mask_index(masks)
 
-            # self.position_enc.requires_grad = self.config.if_update_embed
-            print('embeddings loaded')
+        # Find the target context embeddings, batch_size*max_len*hidden_size
+        masks = masks.type_as(context)
+        masks = masks.expand(hidden_dim, batch_size, max_len).transpose(0, 1).transpose(1, 2)
+        target_emb = masks * context
 
+        target_emb_avg = torch.sum(target_emb, 1) / torch.sum(masks, 1)  # Batch_size*embedding
+        # Expand dimension for concatenation
+        target_emb_avg_exp = target_emb_avg.expand(max_len, batch_size, hidden_dim)
+        target_emb_avg_exp = target_emb_avg_exp.transpose(0, 1)  # Batch_size*max_len*embedding
 
-class Tri_RNNCNNSent(nn.Module):
-    def __init__(self, config):
-        '''
-        In this model, only context words are processed by gated CNN, target is average word embeddings
-        '''
-        super(Tri_RNNCNNSent, self).__init__()
-        self.config = config
+        context = context + target_emb_avg_exp
 
-        # V = config.embed_num
-        D = config.l_hidden_size
-        C = 3  # config.class_num
+        tri_scores = self.feat2tri(context)  # Batch_size*sent_len*2
 
-        Co = 128  # kernel numbers
-        Ks = [2, 3, 4]  # kernel filter size
-        Kt = [2, 3]  # kernel filter size for target words
+        marginals = []
+        select_polarities = []
+        label_scores = []
+        best_latent_seqs = []
+        # Sentences have different lengths, so deal with them one by one
+        for i, tri_score in enumerate(tri_scores):
+            sent_len = lens[i].cpu().item()
+            if sent_len > 1:
+                tri_score = tri_score[:sent_len, :]  # sent_len, 2
+            else:
+                print('Too short sentence')
+            marginal = self.inter_crf(tri_score)  # sent_len, latent_label_size
+            best_latent_seq = self.inter_crf.predict(tri_score)  # sent_len
+            # Get only the positive latent factor
+            select_polarity = marginal[:, 1]  # sent_len, select only positive ones
 
-        self.lstm = MLSTM(config)
+            marginal = marginal.transpose(0, 1)  # 2 * sent_len
+            sent_v = torch.mm(select_polarity.unsqueeze(0),
+                              context[i][:sent_len])  # 1*sen_len, sen_len*hidden_dim=1*hidden_dim
+            label_score = self.feat2label(sent_v).squeeze(0)  # label_size
+            label_scores.append(label_score)
+            best_latent_seqs.append(best_latent_seq)
 
-        self.convs1 = nn.ModuleList([nn.Conv1d(D, Co, K) for K in Ks])
-        self.convs2 = nn.ModuleList([nn.Conv1d(D, Co, K) for K in Ks])
-        self.convs3 = nn.ModuleList([nn.Conv1d(D, Co, K, padding=K - 2) for K in Kt])
+        label_scores = torch.stack(label_scores)
 
-        self.convs4 = nn.ModuleList([nn.Conv1d(Co, Co, K) for K in Ks])
-        self.convs5 = nn.ModuleList([nn.Conv1d(Co, Co, K) for K in Ks])
+        return label_scores, best_latent_seqs
 
-        self.fc_aspect = nn.Linear(len(Kt) * Co, Co)
-
-        self.fc1 = nn.Linear(len(Ks) * Co, C)
-        self.fc2 = nn.Linear(len(Ks) * Co, C)
-        self.fc3 = nn.Linear(len(Ks) * Co, C)
-
-        self.relu = nn.ReLU()
-        self.tanh = nn.Tanh()
-
-        self.cat_layer = SimpleCat(config)
-        self.cat_layer.load_vector()  # init word embeding
-        # self.lambda_unsupervised = nn.Parameter(torch.tensor([0.1]).cuda(),requires_grad = True)
-        # self.lambda_balance= nn.Parameter(torch.tensor([0.0]).cuda(),requires_grad = True)
-        # self.lambda_f = nn.Parameter(torch.tensor([1.0]).cuda(), requires_grad=True)
-
-    def compute_score(self, sents, masks, lens):
+    def forward(self, sents, masks, labels, lens):
         '''
         inputs are list of list for the convenince of top CRF
         Args:
-        sents: a list of sentences， batch_size*max_len*(2*emb_dim)
-        masks: a list of binary to indicate target position, batch_size*max_len
+        sent: a list of sentences， batch_size*len*emb_dim
+        mask: a list of mask for each sentence, batch_size*len
         label: a list labels
         '''
-        # Get the rnn outputs for each word, batch_size*max_len*hidden_size
-        context = self.lstm(sents, lens)
 
-        # Get the target embedding
+        # scores: batch_size*label_size
+        # s_prob:batch_size*sent_len
+        sents = self.cat_layer(sents, masks)
+        scores, s_prob = self.compute_scores(sents, masks, lens)
+        s_prob_norm = torch.stack([s.norm(1) for s in s_prob]).mean()
+
+        pena = F.relu(self.inter_crf.transitions[1, 0] - self.inter_crf.transitions[0, 0]) + \
+               F.relu(self.inter_crf.transitions[0, 1] - self.inter_crf.transitions[1, 1])
+        norm_pen = self.config.C1 * pena + self.config.C2 * s_prob_norm
+
+        scores = F.log_softmax(scores, dim=1)  # Batch_size*label_size
+
+        cls_loss = self.loss(scores, labels)
+
+        print('Transition', pena)
+
+        print("cls loss {0} with penalty {1}".format(cls_loss.item(), norm_pen.item()))
+        return cls_loss + norm_pen
+
+    def predict(self, sents, masks, sent_lens):
+        sents = self.cat_layer(sents, masks)
+        scores, best_seqs = self.compute_predict_scores(sents, masks, sent_lens)
+        _, pred_label = scores.max(1)
+
+        # Modified by Richard Sun
+        return pred_label, best_seqs
+
+    def get_target_emb(self, masks, context):
+        '''
+        Get the embeddings of targets
+        '''
         batch_size, sent_len, dim = context.size()
-
         # Find target indices, a list of indices
         target_indices, target_max_len = convert_mask_index(masks)
+        target_lens = [len(index) for index in target_indices]
+        target_lens = torch.LongTensor(target_lens)
 
         # Find the target context embeddings, batch_size*max_len*hidden_size
         masks = masks.type_as(context)
@@ -188,63 +264,10 @@ class Tri_RNNCNNSent(nn.Module):
         target_embe_squeeze = torch.zeros(batch_size, target_max_len, dim)
         for i, index in enumerate(target_indices):
             target_embe_squeeze[i][:len(index)] = target_emb[i][index]
-        if self.config.if_gpu: target_embe_squeeze = target_embe_squeeze.cuda()
-
-        # Conv input: batch_size * emb_dim * max_len
-        # Conv output: batch_size * out_dim * (max_len-k+1)
-        target_conv = [self.relu(conv(target_embe_squeeze.transpose(1, 2))) for conv in
-                       self.convs3]  # [(N,Co,L), ...]*len(Ks)
-        aa = [F.max_pool1d(a, a.size(2)).squeeze(2) for a in target_conv]  # [(batch_size,Co), ...]*len(Ks)
-        aspect_v = torch.cat(aa, 1)  # N, Co*len(K)
-        aspect_v = self.fc_aspect(aspect_v)
-
-        #         x = [F.tanh(conv(context.transpose(1, 2))) for conv in self.convs1]  # [(N,Co,L), ...]*len(Ks)
-        #         #y = [F.relu(conv(context.transpose(1, 2)) + aspect_v.unsqueeze(2)) for conv in self.convs2]
-        #         y = [F.relu(aspect_v.unsqueeze(2)) for conv in self.convs2]
-        #         x = [i*j for i, j in zip(x, y)] #batch_size, Co, len-1 .  batch_size, Co, len-2
-
-        x = [conv(context.transpose(1, 2)) for conv in self.convs1]  # [(N,Co,L), ...]*len(Ks)
-        y = [F.tanh(conv(context.transpose(1, 2)) + aspect_v.unsqueeze(2)) for conv in self.convs2]
-        # y = [F.sigmoid(aspect_v.unsqueeze(2)) for conv in self.convs2]
-        x = [i * j for i, j in zip(x, y)]  # batch_size, Co, len-1 .  batch_size, Co, len-2
-
-        # pooling method
-        x0 = [F.max_pool1d(i, i.size(2)).squeeze(2) for i in x]  # [(N,out_dim), ...]*len(Ks)
-        x0 = [i.view(i.size(0), -1) for i in x0]  # batch_size*2Co
-
-        sents_vec = torch.cat(x0, 1)  # N*(3*out_dim)
-        # logit = self.fc1(x0)  # (N,C)
-
-        # Dropout
-        if self.training:
-            sents_vec = F.dropout(sents_vec, self.config.dropout)
-
-        output1 = self.fc1(sents_vec)  # Bach_size*label_size
-        output2 = self.fc2(sents_vec)
-        output3 = self.fc3(sents_vec)
-        # output1 = F.log_softmax(output1, dim=1)#Batch_size*label_size
-        # output2 = F.log_softmax(output2, dim=1)
-        f_loss = torch.mean(self.fc1.weight * self.fc2.weight)
-        return output1, output2, output3, f_loss
-
-    def forward(self, sents, masks, labels, lens):
-        # Sent emb_dim
-        sents = self.cat_layer(sents, masks)  # mask embedding
-        # sents, _ = self.cat_layer(sents, masks)
-        # sents = F.dropout(sents, p=0.5, training=self.training)
-        output1, output2, output3, f_loss = self.compute_score(sents, masks, lens)
-
-        # print('Transition', pena)
-        return output1, output2, output3, f_loss
-
-    def predict(self, sents, masks, sent_lens):
-        sents = self.cat_layer(sents, masks)  # mask embedding
-        # sents, _ = self.cat_layer(sents, masks)
-        output1, output2, output3, f_loss = self.compute_score(sents, masks, sent_lens)
-
-        pred_label = torch.max(torch.nn.functional.softmax(output3), dim=1)[1]
-        # Modified by Richard Sun
-        return output1, output2, output3, pred_label
+        if self.config.if_gpu:
+            target_embe_squeeze = target_embe_squeeze.cuda()
+            target_lens = target_lens.cuda()
+        return target_embe_squeeze, target_lens
 
 
 def convert_mask_index(masks):
