0149482ba326343a4ea7b83e69e4ca9528b27ff2
cj555 <chengjuan555@gmail.com>
2019-04-29 11:14:51 +0800

    complete test

--- train_crf_glove.py
+++ train_crf_glove.py
@@ -28,7 +28,7 @@ import pandas as pd
 import util
 from matplotlib import pyplot as plt
 from collections import Counter
-from sklearn.metrics import confusion_matrix, f1_score
+from sklearn.metrics import confusion_matrix, f1_score,accuracy_score
 from sklearn.cluster import KMeans
 import collections
 
@@ -146,29 +146,30 @@ def train1(model, train_by_aspect, test_by_aspect, optimizer, args, tb_logger):
     model.eval()
 
     for target in test_by_aspect.keys():
-        dg_learner_test = data_generator(args, test_by_aspect[target])
         biglearner = models.__dict__['CRFAspectSent'](args, model.bilstm.cuda())
+        final_bigmodel[target] = biglearner
         biglearner.cuda()
         biglearner.train()
-        domain_weight,label_list0, _ = model.predict_domain_weight(target, test_by_aspect)
 
-        domain_weight = (domain_weight - torch.min(domain_weight)) / (torch.max(domain_weight) - torch.min(domain_weight))
-        for idx, k in enumerate(args.aspect_name_ix.keys()):
-            dg_learner_train = data_generator(args, train_by_aspect[k])
-            sent_vecs1, mask_vecs1, label_list1, sent_lens1, _, target_name_list1, _ = next(
-                dg_learner_train.get_ids_samples())
-            label_list1 = torch.LongTensor(label_list1)
-            # weight = domain_weight[idx]
-            weight = torch.mean(domain_weight[:, idx])
-            leaner_loss = biglearner(sent_vecs1.cuda(), mask_vecs1.cuda(), label_list1.cuda(), sent_lens1.cuda())
-            leaner_loss *= (0.5+weight)
-            biglearner.zero_grad()
-            leaner_loss.backward(retain_graph=True)
-            torch.nn.utils.clip_grad_norm_(biglearner.parameters(), args.clip_norm, norm_type=2)
+        for e_ in range(args.epoch)[:1]:
+            domain_weight, label_list0, _ = model.predict_domain_weight(target, test_by_aspect)
 
-        dg_learner_test_eval = data_generator(args, test_by_aspect[target],False)
-        test_acc, test_f1 = evaluate_test(dg_learner_test_eval, biglearner, args)
-        print('test target domain:{0},acc:{1},f1,{2}'.format(target,test_acc,test_f1))
+            for idx, k in enumerate(args.aspect_name_ix.keys()):
+                dg_learner_train = data_generator(args, train_by_aspect[k])
+                sent_vecs1, mask_vecs1, label_list1, sent_lens1, _, target_name_list1, _ = next(
+                    dg_learner_train.get_ids_samples())
+                label_list1 = torch.LongTensor(label_list1)
+                # weight = domain_weight[idx]
+                weight = torch.mean(domain_weight[:, idx])
+                leaner_loss = biglearner(sent_vecs1.cuda(), mask_vecs1.cuda(), label_list1.cuda(), sent_lens1.cuda())
+                leaner_loss *= (0.5 + weight)
+                biglearner.zero_grad()
+                leaner_loss.backward(retain_graph=True)
+                torch.nn.utils.clip_grad_norm_(biglearner.parameters(), args.clip_norm, norm_type=2)
+
+        # dg_learner_test_eval = data_generator(args, test_by_aspect[target],False)
+    test_acc, test_f1 = evaluate_test(final_bigmodel, test_by_aspect, args)
+        # print('test target domain:{0},acc:{1},f1,{2}'.format(target,test_acc,test_f1))
 
 
 def train(model, dg_train, dg_valid, dg_test, optimizer, args, tb_logger, dg_train_cp):
@@ -229,58 +230,53 @@ def train(model, dg_train, dg_valid, dg_test, optimizer, args, tb_logger, dg_tra
         myplot()
 
 
-def evaluate_test(dr_test, model, args, sample_out=False, is_validate=True):
+def evaluate_test(model_dict, test_by_aspect, args, sample_out=False, is_validate=True):
+    logger.info("Evaluting")
     mistake_samples = 'data/mistakes.txt'
     with open(mistake_samples, 'w') as f:
         f.write('Test begins...')
-
-    logger.info("Evaluting")
-    dr_test.reset_samples()
-    model.eval()
-    all_counter = 0
-    correct_count = 0
-    print("transitions matrix ", model.inter_crf.transitions.data)
     all_predictions = []
     all_labels = []
-    while dr_test.index < dr_test.data_len:
-        sent, mask, label, sent_len, texts, targets, _ = next(dr_test.get_ids_samples())
-        pred_label, best_seq = model.predict(sent.cuda(), mask.cuda(), sent_len.cuda())
-        # print(pred_label)
-        # Compute correct predictions
-        correct_count += sum(pred_label == label.cuda()).item()
 
-        ##Output wrong samples, for debugging
-        indices = torch.nonzero(pred_label != label.cuda())
-        if len(indices) > 0:
-            indices = indices.squeeze(1)
-        if sample_out:
-            with open(mistake_samples, 'a') as f:
-                for i in indices:
-                    line = texts[i] + '###' + ' '.join(targets[i]) + '###' + str(label[i]) + '###' + str(
-                        pred_label[i]) + '\n'
-                    f.write(line)
+    for target in model_dict:
+        model = model_dict[target]
+        dr_test = data_generator(args, test_by_aspect[target], False)
+        dr_test.reset_samples()
+        model.eval()
+        all_counter = 0
+        correct_count = 0
+        print("transitions matrix ", model.inter_crf.transitions.data)
 
-        all_predictions.extend(pred_label)
-        all_labels.extend(label)
-    print('Confusion Matrix')
+        while dr_test.index < dr_test.data_len:
+            sent, mask, label, sent_len, texts, targets, _ = next(dr_test.get_ids_samples())
+            pred_label, best_seq = model.predict(sent.cuda(), mask.cuda(), sent_len.cuda())
+            # print(pred_label)
+            # Compute correct predictions
+            correct_count += sum(pred_label == label.cuda()).item()
+
+            ##Output wrong samples, for debugging
+            indices = torch.nonzero(pred_label != label.cuda())
+            if len(indices) > 0:
+                indices = indices.squeeze(1)
+            if sample_out:
+                with open(mistake_samples, 'a') as f:
+                    for i in indices:
+                        line = texts[i] + '###' + ' '.join(targets[i]) + '###' + str(label[i]) + '###' + str(
+                            pred_label[i]) + '\n'
+                        f.write(line)
+
+            all_predictions.extend(pred_label)
+            all_labels.extend(label)
+
+    print('Confusion Matrix:')
     print(confusion_matrix(all_predictions, all_labels))
 
-    acc = correct_count * 1.0 / dr_test.data_len
-    print("Test Sentiment Accuray {0}, {1}:{2}".format(acc, correct_count, all_counter))
-    f1 = f1_score(all_predictions, all_labels, average='macro')
-
+    # acc = correct_count * 1.0 / dr_test.data_len
+    # print("Test Sentiment Accuray {0}, {1}:{2}".format(acc, correct_count, all_counter))
+    f1 = round(f1_score(all_labels, all_predictions, average='macro'),3)
+    acc = round(accuracy_score(all_labels, all_predictions),3)
     # report = classification_report(y_true=all_labels, y_pred=all_predictions, output_dict=True)
-    # report = pd.DataFrame(report).transpose()
-    #
-    # report.index.name = 'index'
-    # report = report.round(3)
-    # print("report:\n{0}".format(report))
-
-    # if is_validate:
-    #     report.to_csv('{}/eval_validate.csv'.format(args.snapshot_dir))
-    # else:
-    #     report.to_csv('{}/eval_test.csv'.format(args.snapshot_dir))
-
+    print('test acc:{acc},f1:{f1}'.format(acc=acc,f1=f1))
     return acc, f1
 
 
@@ -322,11 +318,9 @@ def main():
     args.aspect_name = list(train_by_aspect.keys())
     args.aspect_name_ix = {k: i for i, k in enumerate(args.aspect_name)}
 
-
     model = models.__dict__[args.arch](args)
     parameters = filter(lambda p: p.requires_grad, model.parameters())
     optimizer = create_opt(parameters, args)
-
 
     if args.use_gpu:
         model.cuda()
