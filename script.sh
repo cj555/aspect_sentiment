29cccf5d983051eeed84913bf5ae44b4572a6dc2
cj555 <chengjuan555@gmail.com>
2019-05-17 16:37:58 +0800

    bahs

--- script.sh
+++ script.sh
@@ -1,3 +1,3 @@
 # 20190517
-python train_crf_glove.py --da-grl --exp_name 2 --gpu 1
-python train_crf_glove.py --da-grl --reverse_da_loss --exp_name 1 --gpu 1
+nohup python train_crf_glove.py --da-grl --exp_name 2 --gpu 1 &
+nohup python train_crf_glove.py --da-grl --reverse_da_loss --exp_name 1 --gpu 1 &
