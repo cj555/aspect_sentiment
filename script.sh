9c5b474588d9612c94c6d47bc972794569af91a5
cj555 <chengjuan555@gmail.com>
2019-05-17 17:34:49 +0800

    more experiment

--- script.sh
+++ script.sh
@@ -1,3 +1,4 @@
 # 20190517
 nohup python train_crf_glove.py --da-grl --exp_name 2 --gpu 1 &
 nohup python train_crf_glove.py --da-grl --reverse_da_loss --exp_name 1 --gpu 1 &
+nohup python train_crf_glove.py --exp_name 3 --gpu 0 &
